1ea082f39aa5663b82a8b965fe44b34372fdb383
Scott Stone <scott.allan.stone@gmail.com>
2022-07-07 14:02:33 -0600

    LSL implemented

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,10 @@ use std::time::{Duration, Instant};
 use mouse_rs::Mouse;
 use crossterm::{event::{read, poll, Event, KeyCode}};
 use clap::{Arg, App};
-use winit;
+use lsl::{self, Pushable};
 
-fn main() -> () {
-    let matches = App::new("Mouse Tracker")
-        .version("0.0.1")
-        .author("Scott Stone <scott.allan.stone@gmail.com>")
-        .about("Mouse tracking, but in Rust!")
-        .arg(Arg::with_name("samplerate")
-                 .short('s')
-                 .long("hertz")
-                 .takes_value(false)
-                 .default_missing_value(&"100".to_string())
-                 .help("Mouse sampling rate."))
-        .get_matches();
-
-    let sr = matches.value_of("samplerate").unwrap_or("100").parse::<u64>().unwrap();
-
-    let sample_rate: u64 = sr; // Hz;
-    let update_delay_ms: u64 = 1000 / sample_rate;
-
-    let mouse: Mouse = Mouse::new();
-    let it: Instant = std::time::Instant::now();
-    let mut sample_count: u64 = 0;
-    let mut data: Vec<MousePosition> = Vec::new();
-
-    loop {
-        let mouse_pos = MousePosition::update(&mouse, it);
-        sample_count += 1;
-
-        println!(" > Count:{sc:>6} Time:{ts:>7} X:{x:>5} Y:{y:>5}",
-            sc = sample_count,
-            ts = mouse_pos.timestamp,
-            x  = mouse_pos.x,
-            y  = mouse_pos.y); 
-
-        data.push(mouse_pos);
-        event_handler();
-        
-        thread::sleep(Duration::from_micros(update_delay_ms * 1000));
-    }
-}
+static mut LSL_ENABLE: bool = true;
+static mut DEBUG_OUTPUT: bool = true;
 
 enum State {
     Exit,
@@ -76,6 +39,122 @@ impl MousePosition{
     }
 }
 
+fn main() -> () {
+    let matches = App::new("Mouse Tracker")
+        .version("0.0.1")
+        .author("Scott Stone <scott.allan.stone@gmail.com>")
+        .about("Mouse tracking, but in Rust!")
+        .arg(Arg::with_name("samplerate")
+                 .short('s')
+                 .long("hertz")
+                 .takes_value(false)
+                 .default_missing_value(&"100".to_string())
+                 .help("Mouse sampling rate."))
+
+       /*  .arg(Arg::with_name("lsl")
+                 .short('l')
+                 .long('LSL')
+                 .takes_value(false)
+                 .default_missing_value(true)
+                 .help("Enable LSL streaming. On by default.")) */
+
+        .get_matches();
+    
+    let sr = matches.value_of("samplerate").unwrap_or("100").parse::<u64>().unwrap();
+    
+    let sample_rate: u64 = sr; // Hz;
+    let update_delay_ms: u64 = 1000 / sample_rate;
+
+    let outlet = setup_lsl(
+                        "Mouse Tracker",
+                        "Mouse",
+                        2,
+                        sr as f64,
+                        lsl::ChannelFormat::Int32,
+                        "mouseoutlet1");
+    
+
+    let mouse: Mouse = Mouse::new();
+    let it: Instant = std::time::Instant::now();
+    let mut sample_count: u64 = 0;
+    let mut data: Vec<MousePosition> = Vec::new();
+
+    loop {
+        // Handle events - check if we need to quit etc
+        event_handler();
+
+        // Update mouse position
+        let mouse_pos = MousePosition::update(&mouse, it);
+        sample_count += 1;
+
+        // Send data over LSL, if necessary
+        unsafe {  
+            if LSL_ENABLE {
+                send_lsl(&outlet, &mouse_pos).unwrap();
+            }
+        }
+
+        // Print the data to the console
+        unsafe {
+            if DEBUG_OUTPUT == true {
+                let console_string = format!(" > Count:{:>6} Time:{:>7} X:{:>5} Y:{:>5}\t{:>5}",
+                        sample_count,
+                        mouse_pos.timestamp,
+                        mouse_pos.x,
+                        mouse_pos.y,
+                        check_lsl_enabled());
+                println!("{console_string}");
+            }
+        }
+        // data.push(mouse_pos);
+        
+        thread::sleep(Duration::from_micros(update_delay_ms * 1000)); // adjust to microseconds for more accuracy due to rounding errors
+    }
+}
+
+fn send_lsl(outlet: &lsl::StreamOutlet, data: &MousePosition) -> std::io::Result<()> {
+    unsafe {
+        if LSL_ENABLE {
+            let data = vec![data.x, data.y];
+            outlet.push_sample(&data).unwrap();
+        }
+        Ok(())
+    }
+}
+
+fn check_lsl_enabled() -> &'static str {
+    unsafe {
+        let lsl_enable_str: &str;
+        if LSL_ENABLE == true {
+            lsl_enable_str = "[+lsl]";
+        }
+        else {
+            lsl_enable_str = "[-lsl]";
+        }
+        return lsl_enable_str;
+    }
+}
+
+fn setup_lsl(stream_name: &str,
+             stream_type: &str,
+             channel_count: u32,
+             nominal_srate: f64,
+             channel_format: lsl::ChannelFormat,
+             source_id: &str) -> lsl::StreamOutlet {
+
+    let info = lsl::StreamInfo::new(
+            stream_name,
+            stream_type,
+            channel_count,
+            nominal_srate,
+            channel_format,
+            source_id)
+            .unwrap();
+
+    let outlet = lsl::StreamOutlet::new(&info, 0,360).unwrap();
+    return outlet
+}
+
 fn get_events() -> KeyCode {
     if poll(Duration::from_millis(0)).unwrap() {
         let ev = read().unwrap();
@@ -102,6 +181,23 @@ fn keycode_handler(key: KeyCode) -> State {
         }
         KeyCode::Char('s') => {
             return State::Save;
+        }
+        KeyCode::Char('l') => {
+            unsafe {
+                LSL_ENABLE ^= true;
+                println!(" > LSL output enabled: {:?}", &LSL_ENABLE);
+            }
+            return State::None;
+        }
+        KeyCode::Char('d') => {
+            unsafe {
+                DEBUG_OUTPUT ^= true;
+                if DEBUG_OUTPUT == false {
+                    println!(" > Display mode off. Press 'd' to turn display mode on.");
+                }
+            }
+            return State::None;
+
         }
         _ => {
             return State::None;
